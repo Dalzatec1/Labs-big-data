b6a2ae6b56ad67b5bec226446626631e6558f695
Dalzatec1 <dalzatec1@eafit.edu.co>
2020-11-28 21:31:51 -0500

    lab 2

---
+++ lab2/main.py
@@ -0,0 +1,17 @@
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        vector = line.split(',')
+        empleado=vector[0]
+        salario= int(vector[2])
+        yield empleado,salario
+
+    def reducer(self, key, values):
+        lista=list(values)
+        prom=sum(lista)/len(lista)
+        yield key, prom
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
